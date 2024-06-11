0da1656efbff80c37b7fe680b1a0bfb8e1f6c4c3
Marilisa Moio <moiomarilisa@gmail.com>
2024-06-11 18:02:43 +0200

    feat: add both APIs

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,5 +8,34 @@ use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::with('type', 'technologies')->paginate(3);
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', '=', $slug)->with('type', 'technologies')->first();
+
+        $data = [];
+
+        if($project){
+            $data = [
+                'success' => true,
+                'results' => $project
+            ];
+        } else {
+            $data = [
+                'success' => false,
+                'message' => 'Cannot find a project with this name'
+            ];
+        }
+
+        return response()->json([$data]);
+    }
 }
